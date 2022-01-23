ed25f389e1da2e52fddb0b9a2f10817b358a01e3
JWT <jiewwan@gmail.com>
2022-01-24 00:54:49 +0800

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=jiewwantan/mlrsbostonprices
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+minikube kubectl -- create deployment mlpred-boston-prices --image=jiewwantan/mlrsbostonprices
+
+
+# Step 3:
+# List kubernetes pods
+minikube kubectl -- get pods
+
+# Step 4:
+# Forward the container port to a host
+export POD_NAME=$(minikube kubectl -- get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo POD_NAME: $POD_NAME
+minikube kubectl -- expose deployment/mlpred-boston-prices --type="NodePort" --port 8000
+
+#wait for 5m
+sleep 300
+minikube kubectl -- port-forward pod/$POD_NAME 8000:80
